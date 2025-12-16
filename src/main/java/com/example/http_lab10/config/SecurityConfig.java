4582b781bf65845288663634320ef961e45ed492
Siarhei Kryshchanovich <kryshsi5424_aehit@students.vizja.pl>
2025-12-16 09:26:57 +0100

    Created simple GET ednpoint in HelloController

---
+++ src/main/java/com/example/http_lab10/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.http_lab10.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/hello").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
