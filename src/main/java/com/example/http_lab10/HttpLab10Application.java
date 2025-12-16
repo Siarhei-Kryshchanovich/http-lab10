d68fc71890885fc3b6f8cee6ded8ab3d2a8f82d2
Siarhei Kryshchanovich <kryshsi5424_aehit@students.vizja.pl>
2025-12-16 08:40:15 +0100

    Initial commit

---
+++ src/main/java/com/example/http_lab10/HttpLab10Application.java
@@ -0,0 +1,13 @@
+package com.example.http_lab10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpLab10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpLab10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/http_lab10/HttpLab10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.http_lab10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpLab10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
